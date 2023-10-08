ac6aa39d8fb4b669c5d1611d7ed1c12c47f9e458
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-08 03:29:37 +0200

    Added CustomAttributes

---
+++ src/Helpers/CustomAttibutes.cs
@@ -0,0 +1,23 @@
+﻿using DisCatSharp.ApplicationCommands.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DisCatSharp.ApplicationCommands.Context;
+
+namespace LavaSharp.Attributes
+{
+    public sealed class ApplicationRequireExecutorInVoice : ApplicationCommandCheckBaseAttribute
+    {
+        public ApplicationRequireExecutorInVoice()
+        {
+        }
+
+        public override Task<bool> ExecuteChecksAsync(BaseContext ctx)
+        {
+            return Task.FromResult(ctx.Member.VoiceState?.Channel is not null);
+        }
+
+    }
+}
