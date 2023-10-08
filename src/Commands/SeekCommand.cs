2cf4fbfdbf7adf3d008d7b7590a10c28deb5a783
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-08 14:28:01 +0200

    Added Seeking

---
+++ src/Commands/SeekCommand.cs
@@ -0,0 +1,60 @@
+﻿using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using DisCatSharp.Lavalink;
+using LavaSharp.Attributes;
+using LavaSharp.Helpers;
+
+namespace LavaSharp.Commands;
+
+public class SeekCommand : ApplicationCommandsModule
+{
+    [ApplicationRequireExecutorInVoice]
+    [RequireRunningPlayer]
+    [SlashCommand("seek", "Seeks to a certain time in the song")]
+    public async Task Seek(InteractionContext ctx, [Option("time", "The time to seek to")] string timeString)
+    {
+        if (!TimeParser.TryParseTime(timeString, out TimeSpan time))
+        {
+            var errorEmbed = EmbedGenerator.GetErrorEmbed("Invalid time format. Please use format HH:mm:ss.");
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+            return;
+        }
+
+        var lava = ctx.Client.GetLavalink();
+        var node = lava.ConnectedSessions.First().Value;
+        var player = node.GetGuildPlayer(ctx.Guild);
+        var channel = ctx.Member.VoiceState?.Channel;
+
+        if (player.Channel.Id != channel?.Id)
+        {
+            var errorEmbed = EmbedGenerator.GetErrorEmbed("You must be in the same voice channel as me.");
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+            return;
+        }
+
+        if (player.CurrentTrack == null)
+        {
+            var errorEmbed = EmbedGenerator.GetErrorEmbed("There is no song playing.");
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+            return;
+        }
+
+        if (time > player.CurrentTrack.Info.Length)
+        {
+            var errorEmbed = EmbedGenerator.GetErrorEmbed("You cannot seek past the end of the song.");
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+            return;
+        }
+
+        await player.SeekAsync(time);
+
+        var playEmbed = EmbedGenerator.GetPlayEmbed(player.CurrentTrack);
+        playEmbed.WithDescription($"Seeked to {time:mm\\:ss} in the song.");
+        playEmbed.WithTitle("Seeked");
+
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(playEmbed));
+    }
+}
---
+++ src/Helpers/TimeParser.cs
@@ -0,0 +1,25 @@
+﻿namespace LavaSharp.Helpers;
+
+public class TimeParser
+{
+    public static bool TryParseTime(string input, out TimeSpan time)
+    {
+        time = TimeSpan.Zero;
+        string[] parts = input.Split(':');
+
+        if (parts.Length != 3)
+        {
+            return false;
+        }
+
+        if (int.TryParse(parts[0], out int hours) &&
+            int.TryParse(parts[1], out int minutes) &&
+            int.TryParse(parts[2], out int seconds))
+        {
+            time = new TimeSpan(hours, minutes, seconds);
+            return true;
+        }
+
+        return false;
+    }
+}
