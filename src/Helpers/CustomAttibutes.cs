0c792829200b264ba8465a24729de228928c225b
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-08 03:33:44 +0200

    Fixed Sending and Made check async

--- src/Helpers/CustomAttibutes.cs
+++ src/Helpers/CustomAttibutes.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DisCatSharp.ApplicationCommands.Context;
 using DisCatSharp.Entities;
+using DisCatSharp.Enums;
 using LavaSharp.Config;
 
 namespace LavaSharp.Attributes
@@ -16,16 +17,18 @@ namespace LavaSharp.Attributes
         {
         }
 
-        public override Task<bool> ExecuteChecksAsync(BaseContext ctx)
+        public override async Task<bool> ExecuteChecksAsync(BaseContext ctx)
         {
+            Console.WriteLine("Executing ApplicationRequireExecutorInVoice");
             if (ctx.Member.VoiceState?.Channel is null)
             {
                 var embed = new DiscordEmbedBuilder()
                     .WithDescription("You must be in a voice channel to use this command.")
                     .WithColor(BotConfig.GetEmbedColor());
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AddEmbed(embed));
+                return false;
             }
-
-            return Task.FromResult(ctx.Member.VoiceState?.Channel is not null);
+            return true;
         }
 
     }
