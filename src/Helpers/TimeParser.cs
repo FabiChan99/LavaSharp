f3efea28b0705a8bb53f6b8609d10c0ef212694e
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-08 14:56:29 +0200

    Updated Timeparser

--- src/Helpers/TimeParser.cs
+++ src/Helpers/TimeParser.cs
@@ -1,25 +1,25 @@
-﻿namespace LavaSharp.Helpers;
-
-public class TimeParser
+﻿namespace LavaSharp.Helpers
 {
-    public static bool TryParseTime(string input, out TimeSpan time)
+    public class TimeParser
     {
-        time = TimeSpan.Zero;
-        string[] parts = input.Split(':');
-
-        if (parts.Length != 2 && parts.Length != 3)
+        public static bool TryParseTime(string input, out TimeSpan time)
         {
+            time = TimeSpan.Zero;
+            string[] parts = input.Split(':');
+
+            if (parts.Length != 2)
+            {
+                return false;
+            }
+
+            if (int.TryParse(parts[0], out int minutes) &&
+                int.TryParse(parts[1], out int seconds))
+            {
+                time = new TimeSpan(0, minutes, seconds);
+                return true;
+            }
+
             return false;
         }
-
-        if (int.TryParse(parts[0], out int hours) &&
-            int.TryParse(parts[1], out int minutes))
-        {
-            int seconds = parts.Length == 3 ? (int.TryParse(parts[2], out seconds) ? seconds : 0) : 0;
-            time = new TimeSpan(hours, minutes, seconds);
-            return true;
-        }
-
-        return false;
     }
 }
