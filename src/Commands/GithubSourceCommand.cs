2456ffb9341cec833f4e50dde68db70586ae7f13
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-15 17:37:13 +0200

    Added "Source" command to show the link to the github repo

---
+++ src/Commands/GithubSourceCommand.cs
@@ -0,0 +1,22 @@
+﻿using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+
+namespace LavaSharp.Commands;
+
+public class GithubSourceCommand : ApplicationCommandsModule
+{
+    [SlashCommand("source", "Shows the source code for the bot.")]
+    public static async Task Github(InteractionContext ctx)
+    {
+        var embed = new DiscordEmbedBuilder();
+        var link = "https://github.com/FabiChan99/LavaSharp";
+        embed.WithTitle("This project is open source!");
+        embed.WithDescription($"You can find the source code for this bot at {link}.\n You can host your own instance of this bot if you want to.");
+        embed.WithColor(DiscordColor.Blurple);
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().AddEmbed(embed));
+    }
+}
