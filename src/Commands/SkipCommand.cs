38a51bf757a89223c9512c5d3f6209e8e4fa7996
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-15 16:50:59 +0200

    Reworked skip command to allow to skip multiple tracks

--- src/Commands/SkipCommand.cs
+++ src/Commands/SkipCommand.cs
@@ -6,6 +6,7 @@ using DisCatSharp.ApplicationCommands.Context;
 using DisCatSharp.Entities;
 using DisCatSharp.Enums;
 using DisCatSharp.Lavalink;
+using DisCatSharp.Lavalink.Entities;
 using LavaSharp.Attributes;
 using LavaSharp.Helpers;
 using LavaSharp.LavaManager;
@@ -21,7 +22,9 @@ namespace LavaSharp.Commands
         [ApplicationRequireExecutorInVoice]
         [RequireRunningPlayer]
         [SlashCommand("skip", "Skips the current song")]
-        public async Task Skip(InteractionContext ctx)
+        public async Task Skip(InteractionContext ctx,
+            [Option("number_of_tracks", "[Optional] The number of tracks to skip")]
+            int tracksToSkip = 1)
         {
             var queue = LavaQueue.queue;
             var lava = ctx.Client.GetLavalink();
@@ -45,14 +48,46 @@ namespace LavaSharp.Commands
                 return;
             }
 
-            var track = queue.Dequeue();
+            if (tracksToSkip > queue.Count || tracksToSkip < 1)
+            {
+                var errorEmbed =
+                    EmbedGenerator.GetErrorEmbed(
+                        $"Number of tracks to skip must be between 1 and the queue length ({queue.Count}).");
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+                return;
+            }
 
-            CurrentPlayData.track = track.Item1;
-            CurrentPlayData.user = track.Item2;
-            await player.PlayAsync(track.Item1);
-            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
-                new DiscordInteractionResponseBuilder().WithContent("\u23e9 | Skipped the current song."));
-            await ctx.Channel.SendMessageAsync(EmbedGenerator.GetCurrentTrackEmbed(track.Item1, player));
+            if (tracksToSkip == 1)
+            {
+                var track = queue.Dequeue();
+
+                CurrentPlayData.track = track.Item1;
+                CurrentPlayData.user = track.Item2;
+                await player.PlayAsync(track.Item1);
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent("⏭️ | Skipped the current song."));
+                await ctx.Channel.SendMessageAsync(EmbedGenerator.GetCurrentTrackEmbed(track.Item1, player));
+                return;
+            }
+            if (tracksToSkip >= 2)
+            {
+                await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                    new DiscordInteractionResponseBuilder().WithContent($"⏭️ | Skipping ``{tracksToSkip}`` songs....."));
+                LavalinkTrack targettrack = null;
+                for (int i = 0; i < tracksToSkip; i++)
+                {
+                    var track = queue.Dequeue();
+                    CurrentPlayData.track = track.Item1;
+                    CurrentPlayData.user = track.Item2;
+                    targettrack = track.Item1;
+                }
+                await player.PlayAsync(targettrack);
+                await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent($"⏭️ | Skipped ``{tracksToSkip}`` songs."));
+                await ctx.Channel.SendMessageAsync(EmbedGenerator.GetCurrentTrackEmbed(CurrentPlayData.track, player));
+                return;
+            }
+
         }
-    }
+    }   
 }
