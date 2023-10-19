a68dd2a9a7d4e750c085a5b875d2d359261ea66f
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-10-19 17:29:42 +0200

    Added ReplayCommand.cs

---
+++ src/Commands/ReplayCommand.cs
@@ -0,0 +1,49 @@
+﻿using DisCatSharp.ApplicationCommands;
+using DisCatSharp.ApplicationCommands.Attributes;
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Entities;
+using DisCatSharp.Enums;
+using DisCatSharp.Lavalink;
+using LavaSharp.Attributes;
+using LavaSharp.Helpers;
+
+namespace LavaSharp.Commands;
+
+public class ReplayCommand: ApplicationCommandsModule
+{
+    [EnsureGuild]
+    [EnsureMatchGuildId]
+    [ApplicationRequireExecutorInVoice]
+    [RequireRunningPlayer]
+    [CheckDJ]
+    [SlashCommand("replay", "Replays the current track.")]
+    public static async Task Replay(InteractionContext ctx)
+    {
+        var lava = ctx.Client.GetLavalink();
+        var node = lava.ConnectedSessions.First().Value;
+        var player = node.GetGuildPlayer(ctx.Guild);
+        var channel = ctx.Member.VoiceState?.Channel;
+
+        if (player?.Channel.Id != channel?.Id)
+        {
+            var errorEmbed = EmbedGenerator.GetErrorEmbed("You must be in the same voice channel as me.");
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+            return;
+        }
+
+        if (player?.CurrentTrack == null)
+        {
+            var errorEmbed = EmbedGenerator.GetErrorEmbed("There is no song playing.");
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+                new DiscordInteractionResponseBuilder().AddEmbed(errorEmbed));
+            return;
+        }
+
+        await player.PlayAsync(player.CurrentTrack);
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,
+            new DiscordInteractionResponseBuilder().WithContent(
+                $"🔂 | Replaying the current track."));
+        await NowPlaying.sendNowPlayingTrack(ctx, player.CurrentTrack);
+    }
+}
